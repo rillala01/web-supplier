b6b4b32c77606fd0d8e87d5f19d3988f4c2b589a
bayudani <bayu22122017@gmail.com>
2025-12-16 12:42:01 +0700

    update heading of PenjualanChart widget for clarity

--- app/Filament/Widgets/PenjualanChart.php
+++ app/Filament/Widgets/PenjualanChart.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class PenjualanChart extends ChartWidget
 {
-    protected static ?string $heading = 'Grafik Penjualan';
+    protected static ?string $heading = 'Grafik Penjualan perbulan';
 
     protected function getData(): array
     {
